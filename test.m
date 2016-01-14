f8a2bdac3689ea86a695f4eba1b34942477a06da
Noah Stier <noah.stier@gmail.com>
2016-01-14 12:39:53 -0800

    add test data

--- test.m
+++ test.m
@@ -1,13 +1,15 @@
-load 10a.mat a_frangi b_frangi
+load testdata
 
-k = 20;
-N = 100;
+k = 20; % kernel size
+N = 100; % point sample size
 
-[aPts(:, 1), aPts(:, 2)] = find(a_frangi);
-[bPts(:, 1), bPts(:, 2)] = find(b_frangi);
+% take random sample of points
+[aPts(:, 1), aPts(:, 2)] = find(A);
+[bPts(:, 1), bPts(:, 2)] = find(B);
 R1 = randperm(length(aPts));
 R2 = randperm(length(bPts));
 aPts_sample = aPts(R1(1:N), :);
 bPts_sample = bPts(R2(1:N), :);
 
+% perform registration
 T = LKMReg(aPts_sample, bPts_sample, k);
