c0f02b02185310a164f028f0a42f21d801991572
dsaReg <noah.stier@gmail.com>
2016-01-21 16:57:38 -0800

    test and train on different sets

--- demo.m
+++ demo.m
@@ -2,9 +2,9 @@ load testdata
 
 % Train the model
 
-ims{1}.A = A;
-ims{1}.B = B;
-ims{1}.groundtruth = groundtruth;
+ims{1}.A = patient2A;
+ims{1}.B = patient2B;
+ims{1}.groundtruth = patient2groundtruth;
 
 k = 50;
 N = 500;
@@ -12,4 +12,4 @@ N = 500;
 mlModel = LKM.trainModel(ims, k, N);
 
 % perform registration
-T = LKM.register(A, B, k, N, mlModel, true);
+T = LKM.register(patient1A, patient1B, k, N, mlModel, true);
